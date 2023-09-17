564201be100acf2bdf935e7668489506d103ab37
Zaidhamza90 <138580843+Zaidhamza90@users.noreply.github.com>
2023-09-17 16:10:46 +0000

    conversion of data types

---
+++ .devcontainer/01_basic/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valuenNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; fasle =>0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Zaidhamza" => true
+
+let someNumber = 3
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
